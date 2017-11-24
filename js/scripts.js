f3686051c2657d8e16bc43ea713a31b497d68689
Zuzanna <zuzanna.zuzewicz@gmail.com>
2017-11-24 13:51:39 +0200

    Updated scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 function getTriangleArea (a, h) {
 	if (a <= 0 || h <= 0) {
 		return console.log("Nieprawidłowe dane")
-	} else return a * h / 2;
+	} else return (a * h) / 2;
 }
 
 var triangle1Area = getTriangleArea(43, 20);
-var	triangle2Area = getTriangleArea(0, 12);
-var	triangle3Area = getTriangleArea(8723, 0);
+var triangle2Area = getTriangleArea(0, 12);
+var triangle3Area = getTriangleArea(8723, 0);
 
 console.log(getTriangleArea(24, 6));
 console.log(triangle1Area);
